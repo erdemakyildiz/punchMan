326be43b96c84f8a51848e6fe4d0adc0e0eed9bd
erdemakyildiz <erdem.akyildiz@hibizz.com>
2018-12-31 17:16:08 +0300

    .

--- main.lua
+++ main.lua
@@ -1,80 +1,47 @@
-Camera = require 'Camera'
-local animator = require 'animator'
+-- global
+Object = require("library/classic")
+Camera = require("library/Camera")
+Animator = require("library/Animator")
+Push = require("library/Push")
 
-local cameraSpeed = 1000
+-- props
+cameraSpeed = 1000
+
+virtualWidth = 1280
+virtualHeight = 720
+
+windowWidth = 1280
+windowHeight = 720
 
 function love.load()
-  grass = love.graphics.newImage("grass.png")
-  
-  block_width = grass:getWidth()
-  block_height = grass:getHeight()
-  block_depth = block_height / 2
-  
-  camera = Camera()
-  
-  tileW = 85
-  tileH = 170
-  
-  local image = love.graphics.newImage( 'atlas.png' )
-  local one = love.graphics.newQuad( 0, 0, tileW, tileH, image:getWidth(), image:getHeight() )
-  local two = love.graphics.newQuad( tileW, 0, tileW, tileH, image:getWidth(), image:getHeight() )
-  local three = love.graphics.newQuad( tileW * 2, 0, tileW, tileH, image:getWidth(), image:getHeight() )
-  local four = love.graphics.newQuad( tileW * 3, 0, tileW, tileH, image:getWidth(), image:getHeight() )
-  local five = love.graphics.newQuad( tileW * 4, 0, tileW, tileH, image:getWidth(), image:getHeight() )
-  local six = love.graphics.newQuad( tileW * 5, 0, tileW, tileH, image:getWidth(), image:getHeight() )
-  
-	anim = animator.newAnimation( { two, two, three, four, five, six }, 0.05, image )
-  anim:setLooping()
+   camera = Camera()
+
+   require("Map")
+   Map = Map()
+
+   --require("Player")
+  -- Player = Player()
+
+   Push:setupScreen(virtualWidth, virtualHeight, windowWidth, windowHeight, {
+      fullscreen = false,
+      resizable = true
+    })
 end
 
 function love.update(dt)
-    anim:update( dt )
     camera:update(dt)
-  
-    if love.keyboard.isDown("a") then
-       camera.x = camera.x - cameraSpeed * dt
-    end
-    
-    if love.keyboard.isDown("d") then
-       camera.x = camera.x + cameraSpeed * dt
-       
-    end
-    
-    if love.keyboard.isDown("w") then
-       camera.y = camera.y - cameraSpeed * dt
-    end
-    
-    if love.keyboard.isDown("s") then
-       camera.y = camera.y + cameraSpeed * dt
-    end
-    
-    if love.keyboard.isDown("f") then
-       camera.scale = camera.scale + 0.05
-    end
-    
-    if love.keyboard.isDown("c") then
-       camera.scale = camera.scale - 0.05
-    end
-      
+    Map:update(dt)
+    --Player:update(dt)
 end
 
 function love.draw()
    camera:attach()
-    -- Draw your game here
-    
-  love.graphics.setColor(1,1,1,1)
-  
-  
-   for x = 1,20 do
-     for y = 1,20 do
-          love.graphics.draw(grass,
-             ((y-x) * (block_width / 2 - 10)),
-            ((x+y) * (block_depth / 2 + 10)) - (block_depth))
-     end
-   end
-   
-   anim:draw()
+ 
+   love.graphics.setColor(1,1,1,1)
+
+   Map:render()
+   -- Player:render()
    
    camera:detach()
-    camera:draw() 
+   camera:draw() 
 end
